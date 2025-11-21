5af842bd702c0d9ab541ef916a5b9d289a8605eb
Ibrahim Drame <ibrahim@macbook-air-de-ibrahim-2-1.home>
2025-11-21 16:35:55 +0100

    ajout boutons et titres stylisé sur la landing page

--- src/app/Components/header-component/header-component.ts
+++ src/app/Components/header-component/header-component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Buttons } from '../../shared/buttons/buttons';
 
 @Component({
   selector: 'app-header-component',
-  imports: [],
+  imports: [Buttons, CommonModule], 
   templateUrl: './header-component.html',
   styleUrl: './header-component.css',
 })
 export class HeaderComponent {
 
   urlImageSushi : string = "https://sushitaro.fr/wp-content/uploads/2025/04/quel-est-le-meilleur-restaurant-de-sushi-en-France.png"
-
+  commandeButton : string = "Commander"
+  
+  // Classes pour le titre avec ngClass
+  titleClasses: string = "font-['Protest_Revolution'] text-[#EC3C3C] text-[9rem] text-center [text-shadow:2px_2px_4px_rgba(0,0,0,0.5)]";
 }
--- src/app/shared/buttons/buttons.ts
+++ src/app/shared/buttons/buttons.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-buttons',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './buttons.html',
   styleUrl: './buttons.css',
 })
 export class Buttons {
-
+  commandeButton : string = "Commander"
+  buttonClasses: string = "bg-[#DEBA97] text-white px-20 py-3 rounded-2xl hover:bg-[#C49564] transition-colors duration-300 cursor-pointer";
+  
+  constructor(){
+    this.commandeButton = "Commander";
+  }
 }
