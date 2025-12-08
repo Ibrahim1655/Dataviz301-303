1bcae024e439c7dc07af9ac9d5d9d79101d6227e
Ibrahim1655 <ibrahimdrame165@gmail.com>
2025-12-08 12:19:27 +0100

    Navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { Landing } from './pages/landing/landing';
 import { Component } from 'lucide-angular';
 import { About } from './pages/about/about';
 import { Menu } from './menu/menu';
+import { Contact } from './pages/contact/contact';
+import { PanierComponent } from './shared/panier-component/panier-component';
 
 export const routes: Routes = [
     {path:'', component:Landing},
     {path:'About', component:About},
-    {path:'Menu', component:Menu}
+    {path:'Menu', component:Menu},
+    {path:'Contact', component:Contact},
+    {path:'Panier', component:PanierComponent}
     
 ];
