00fe80bd880642f42d328fc8fc510b912171dbaa
590kurtis <savourinthomas@gmail.com>
2025-12-16 12:27:57 +0100

    landing

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../../shared/navbar/navbar";
 import { HeaderComponent } from "../../Components/header-component/header-component";
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet, RouterLink],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
 })
