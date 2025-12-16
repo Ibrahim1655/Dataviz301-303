21ca4b663e127b83666376365cabe6cb10fb05c6
590kurtis <savourinthomas@gmail.com>
2025-12-16 09:48:48 +0100

    panier

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { Navbar } from "../../shared/navbar/navbar";
 import { HeaderComponent } from "../../Components/header-component/header-component";
 import { RouterOutlet } from '@angular/router';
-import { Menu } from '../../menu/menu';
+
 
 @Component({
   selector: 'app-landing',
-  standalone:true,
-  imports: [ HeaderComponent, RouterOutlet, Menu],
+  standalone: true,
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
 })
