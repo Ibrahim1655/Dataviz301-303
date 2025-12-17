9fb09e8da504405a24b0e13d227676bb52d3a104
Ibrahim1655 <ibrahimdrame165@gmail.com>
2025-12-18 00:34:08 +0100

    animation lors du paiement + sauvegarde du panier dans le localstorage

--- src/app/panier-service.ts
+++ src/app/panier-service.ts
@@ -1,23 +1,38 @@
-import { computed, Injectable } from '@angular/core';
+import { computed, Injectable  , signal , effect} from '@angular/core';
 import { Box } from './Models/iboxes';
-import { signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PanierService {
-  //initialisation du panier vide
 
-  articles = signal<Box[]>([])
+private readonly STORAGE_KEY = 'panier_sushi_crousty';
 
-  total = computed(()=>{
+  
+  // On essaye de lire le localStorage immédiatement lors de la création du signal
+  public articles = signal<Box[]>(this.getPanierFromStorage());
+
+  total = computed(() => {
     return this.articles().reduce((acc, Box)=>acc + Box.price,0)
   })
 
 
-  nombreArticle = computed(()=>{
+  nombreArticle = computed(()=> {
     return this.articles().length
   })
+
+  constructor(){
+    // L'effect se lance à chaque fois que 'this.articles()' change.
+    // dans le constructeur pourque le panier soit surveillé toute la durée de vie du service 
+    effect(() => {
+      const data = JSON.stringify(this.articles());
+      localStorage.setItem(this.STORAGE_KEY, data);
+      
+      console.log('Sauvegarde auto effectuée !'); // Pour vérifier dans la console
+    });
+    
+    
+  }
 
   ajouter(box:Box){
     this.articles.update(liste =>[...liste , box])
@@ -30,6 +45,16 @@ export class PanierService {
   vider(){
     this.articles.set([])
   }
+  private getPanierFromStorage(): Box[] {
+    
+    if (typeof localStorage !== 'undefined') {
+      const articleDuPanier = localStorage.getItem(this.STORAGE_KEY);
+      // Si des données existent, on les transforme en objet, sinon tableau vide
+      return articleDuPanier ? JSON.parse(articleDuPanier) : [];
+    }
+    return [];
+  }
+}
 
   
 
@@ -38,4 +63,4 @@ export class PanierService {
 
 
   
-}
+
--- src/app/panier/panier.ts
+++ src/app/panier/panier.ts
@@ -3,17 +3,28 @@ import { PanierService } from '../panier-service';
 import { Box } from '../Models/iboxes';
 import { inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-panier',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, LucideAngularModule],
   templateUrl: './panier.html',
   styleUrl: './panier.css',
 })
 export class Panier {
+
+
+  
   panierService = inject(PanierService);
+  router = inject(Router)
+
+  isLoading = false;// le client est il en train de payer
+  isSuccess =false;// la paiement est il validé
+
+
+
 
 
   supprimerArticle(box: Box){
@@ -22,5 +33,20 @@ export class Panier {
 
   viderPanier(){
     this.panierService.vider();
+  }
+
+  passerCommande(){
+    // pour lancer l'animation de chargement
+    this.isLoading = true;
+    
+    // on attent 2 secondes pour simuler la banque
+    setTimeout(()=>{
+      // c'est fini
+      this.isLoading = false;
+      this.isSuccess = true;
+
+      // on vide la panier 
+      this.panierService.vider();
+    },2000)
   }
 }
